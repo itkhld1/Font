9699bb99843a13627c6d0c9250c90d10dc3fbb65
Khaled Samim <asamim20@posta.pau.edu.tr>
2024-10-30 17:41:26 +0300

    Initial Commit

---
+++ Font/FontApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontApp.swift
+//  Font
+//
+//  Created by itkhld on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct FontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
